1127c33666ab9886c38e90751319aa9dc212298b
EAWAG\schollje <jennifer.schollee@eawag.ch>
2018-02-08 10:58:46 +0100

    Updated to remove xic function to ensure compatability with latest MSnBase version. Added option for RT restriction on EIC plot

--- EIC&MSMS_shiny.R
+++ EIC&MSMS_shiny.R
@@ -19,11 +19,11 @@ ui <- fluidPage(
       selectInput(inputId = "masscol", label = "Column with precursor masses", choices = c()),
       selectInput(inputId = "rtcol", label = "Column with RTs", choices = c()),
       selectInput(inputId = "rtunit", label = "Retention time units", choices = c("minutes", "seconds")),
+      checkboxInput(inputId = "useRT", "Use retention time for feature extraction"),
       selectInput(inputId = "IDcol", label = "Column with ID", choices = c()),
       selectInput(inputId = "ioncol", label = "Column with ionization mode", choices = c()),
       selectInput(inputId = "ionization", label = "Which Ionization mode?", choices = c()),
       
-      selectInput(inputId = "feature", label = "feature to extract", choices = c()), ## filled with masses from P_data
       numericInput(inputId = "masstol", label = "ppm tolerence", value = c(10)),
       numericInput(inputId = "rtwind", label = "rt window (sec)", value = c(60)), ## user selected
       
@@ -40,28 +40,35 @@ ui <- fluidPage(
         tabPanel("Data", tableOutput(outputId = "userP_data")),
         
         tabPanel("TIC", 
-          plotOutput(outputId = "TIC"),
-          actionButton(inputId = "create2d", label = "Create 2d plot"),
-          plotOutput(outputId = "plot2d"),
-          selectInput(inputId = "coloring", label = "Color points according to:", choices = c("ionCount", "file", "peaks.count", "charge")),
-          selectInput(inputId = "RAWFileTIC", label = "Current mzXML file:", choices = c(), multiple = FALSE) ##  filled with names in filedir
-          ),
+                 actionButton(inputId = "createTIC", label = "Create TIC"),
+                 plotOutput(outputId = "TIC"),
+                 actionButton(inputId = "create2d", label = "Create 2d plot"),
+                 plotOutput(outputId = "plot2d"),
+                 selectInput(inputId = "coloring", label = "Color points according to:", choices = c("ionCount", "file", "peaks.count", "charge")),
+                 selectInput(inputId = "RAWFileTIC", label = "Current mzXML file:", choices = c(), multiple = FALSE) ##  filled with names in filedir
+                 ),
         
         tabPanel("EIC", 
+                 actionButton(inputId = "createEIC", label = "Create EIC"),
+                 selectInput(inputId = "feature", label = "feature to extract", choices = c()), ## filled with masses from P_data
                  textOutput(outputId = "Compound"),
-                 plotOutput(outputId = "EIC", inline = TRUE),
+                 plotOutput(outputId = "EIC"),
                  selectInput(inputId = "RAWFileEIC", label = "Current mzXML file:", choices = c(), multiple = FALSE) ##  filled with names in filedir
-          ),
+                 ),
+        
+        tabPanel("MS2 Spectra",
+                 selectInput(inputId = "MS2feature", label = "feature to extract", choices = c()), ## filled with masses from P_data
+                 plotOutput(outputId = "MS2plot"),
+                 selectInput(inputId = "RAWFileMS2", label = "Current mzXML file:", choices = c(), multiple = FALSE) ##  filled with names in filedir
+                 ),
         
         tabPanel("Data Output", 
-          
-          checkboxGroupInput(inputId = "RAWFileOutput", label = "Files to process:", choices = c(), inline = TRUE), ##  filled with names in filedir
-          actionButton("selectall", "Select All Files"),
-          #checkboxInput(inputId = "pdf", label = "Save images to pdf?"), 
-          textInput(inputId = "outputdir", label = "Directory to save pdf:"),
-          actionButton(inputId = "process", label = "Save to pdf")
-
-        )
+                 checkboxGroupInput(inputId = "RAWFileOutput", label = "Files to process:", choices = c(), inline = TRUE), ##  filled with names in filedir
+                 actionButton("selectall", "Select All Files"),
+                 #checkboxInput(inputId = "pdf", label = "Save images to pdf?"), 
+                 textInput(inputId = "outputdir", label = "Directory to save pdf:"),
+                 actionButton(inputId = "process", label = "Save to pdf")
+                 )
       )
     )
   )
@@ -82,6 +89,27 @@ server <- function(input, output, session){
     return(wind)
   }
   
+  rtbounds <- function(exp.rt, rtwind){
+    
+    rtmin <- exp.rt - rtwind
+    rtmax <- exp.rt + rtwind
+    return(c(rtmin, rtmax))
+  }
+  
+  findMS2 <- function(hd.ms1, hd.ms2, eic, precursor.mz, mztol){
+    
+    mzlimits <- ppm(precursor.mz, mztol, l = TRUE)
+    ms2.scans <- which(hd.ms2$precursorMZ < mzlimits[1] & hd.ms2$precursorMZ > mzlimits[2])
+    if(length(ms2.scans)==0){return("NA")}
+    ms1.scans <- sapply(ms2.scans, function(x) which.min(abs(hd.ms1$acquisitionNum - hd.ms2$acquisitionNum[x])))
+    
+    eic.int <- eic@.Data[[1]]@intensity[ms1.scans]
+    ms2.rt <- hd.ms2$retentionTime[ms2.scans]
+    return(cbind(ms2.rt, eic.int))
+    
+  }
+  
+  
   output$userP_data <- renderTable({
     P_data()
   })
@@ -97,8 +125,10 @@ server <- function(input, output, session){
                        filename = c(),
                        ftic = c(),
                        hdtic = c(),
-                       fteic = c(),
-                       hdeic = c(),
+                       feic.ms1 = c(),
+                       hdeic.ms1 = c(),
+                       feic.ms2 = c(),
+                       hdeic.ms2 = c(),
                        allcalc = c(),
                       # mzmin = c(),
                       # mzmax = c(),
@@ -176,107 +206,168 @@ server <- function(input, output, session){
   })
   
 
-  observeEvent(input$RAWFileTIC,{
+  observeEvent(input$createTIC,{
 
       process.file1 <- which(rv$filename == input$RAWFileTIC)
-
-      rv$ftic <- try(openMSfile(rv$filepath[process.file1]))
-      rv$htic <- try(header(rv$ftic))
-
-      output$TIC <- renderPlot({
-        chromatogram(rv$ftic)
-      })
       
-      observeEvent(input$create2d,{
-        spec <- try(readMSData(rv$filepath[process.file1]))
-        output$plot2d <- renderPlot({
-          plot2d(spec, z  = input$coloring)
-      })
+      #browser()
+      
+      if(length(process.file1) == 0){output$status <- renderText({"Waiting for mzXML files..."})}
+      
+      else{
+        
+        output$status <- renderText({"Plotting TIC"})
+        rv$ftic <- try(readMSData(rv$filepath[process.file1], mode = "onDisk"))
+        rv$htic <- tryCatch(header(rv$ftic))
+        
+        output$status <- renderText({"Read in file"})
+        output$TIC <- renderPlot({
+          plot(chromatogram(rv$ftic))
+        })
+        
+        output$status <- renderText({"Finished"})
 
-      #  plot(MSmap(rv$ftic, lowMz = min(rv$masses), highMz = max(rv$masses), resMz = ppm(max(rv$masses), input$masstol, p = TRUE)), aspect = 1, allTicks = FALSE)
-      })
-  })
+        observeEvent(input$create2d,{
+          spec <- tryCatch(readMSData(rv$filepath[process.file1], msLevel. = 1, centroided. = TRUE))
+          
+          output$status <- renderText({"Plotting 2d"})
+          
+          output$plot2d <- renderPlot({
+            plot2d(spec, z  = input$coloring)
+          })
+          
+          output$status <- renderText({"Finished 2d"})
+
+          #  plot(MSmap(rv$ftic, lowMz = min(rv$masses), highMz = max(rv$masses), resMz = ppm(max(rv$masses), input$masstol, p = TRUE)), aspect = 1, allTicks = FALSE)
+        })
+      }
+
+  });
   
   
-  observeEvent(c(input$RAWFileEIC, input$feature),{
-    
+  observeEvent(input$createEIC,{
+
     process.file2 <- which(rv$filename == input$RAWFileEIC)
     process.feature <- which(rv$features == input$feature)
-    rtlim.small <- rv$rts_sec[process.feature] - input$rtwind
-    rtlim.large <- rv$rts_sec[process.feature] + input$rtwind
+    
+    if(length(process.file2) == 0){output$status <- renderText({"Waiting for mzXML files..."})}
+    if(length(process.feature) == 0){output$status <- renderText({"Waiting for suspect list..."})}
+    
+    
+  #  rtlim.small <- rv$rts_sec[process.feature] - input$rtwind
+  #  rtlim.large <- rv$rts_sec[process.feature] + input$rtwind
   #  ppm <- ppm(rv$masses[process.feature], input$masstol, p = T)
   #  mzwidth <- as.numeric(rv$mzmax[process.feature]) - as.numeric(rv$mzmin[process.feature])
-    
-    
-    rv$feic <- try(openMSfile(rv$filepath[process.file2]))
-    rv$heic <- try(header(rv$feic))
 
-        output$EIC <- renderPlot({
+    if(length(process.file2) == 1){
       
-      xic(rv$feic, mz = rv$masses[process.feature], 
-         # rtlim = c(rtlim.small, rtlim.large), 
-          width = mzWindow(rv$masses[process.feature], input$masstol),
-         points = TRUE, legend = TRUE, main = rv$features[process.feature])
-      abline(v = rv$rts_sec[process.feature], col = "blue", lwd = 2)
-          
-    }, height = 1000, width = 1200
-    )
-        
-        output$Compound <- renderText({paste(rv$names[process.feature])})
-  })
-  
-  
-  observeEvent(c(input$RAWFileOutput,input$selectall), {
+      output$status <- renderText({"Ready to plot EIC..."})
     
-    if(input$selectall == 0){
-      rv$allcalc <- input$RAWFileOutput
-      
-    }else if(input$selectall%%2 == 0){
-      updateCheckboxGroupInput(session, "RAWFileOutput", choices = rv$filename)
-      rv$allcalc <- input$RAWFileOutput
-      
+    rv$feic.ms1 <- try(readMSData(rv$filepath[process.file2], centroided. = TRUE, msLevel. = 1, mode = "onDisk"))
+    rv$hdeic.ms1 <- try(header(rv$feic.ms1))
+
+    rv$feic.ms2 <- try(readMSData(rv$filepath[process.file2], centroided. = TRUE, msLevel. = 2, mode = "onDisk"))
+    rv$hdeic.ms2 <- try(header(rv$feic.ms2))
+
+    output$EIC <- renderPlot({
+
+    if(input$useRT == TRUE){
+
+      eic <- chromatogram(rv$feic.ms1, rt = rtbounds(rv$rts_sec[process.feature], input$rtwind), mz = ppm(rv$masses[process.feature], input$masstol, l = TRUE))
+      plot(eic)
+      abline(v = rv$rts_sec[process.feature], col = "blue")
+      ms2.data <- findMS2(rv$hdeic.ms1, rv$hdeic.ms2, eic, rv$masses[process.feature], input$masstol)
+      if(is.na(ms2.data)){legend("topleft", legend = paste("No MS2 scans"))}
+      points(ms2.data, col = "red", pch = 16)
+
     }else{
-      updateCheckboxGroupInput(session, "RAWFileOutput", choices = rv$filename, selected = rv$filename)
-      rv$allcalc <- input$RAWFileOutput
       
-    }
-    
-  })
-  
-  
-  observeEvent(input$process,{
-    
-    #output.dir <- input$outputdir
-    
-    for(i in 1:length(rv$allcalc)){
+      eic <- chromatogram(rv$feic.ms1, rt = rtbounds(rv$rts_sec[process.feature], input$rtwind), mz = ppm(rv$masses[process.feature], input$masstol, l = TRUE))
+      ms2.data <- findMS2(rv$hdeic.ms1, rv$hdeic.ms2, eic, rv$masses[process.feature], input$masstol) ### problem with plotting ms2 scans comes from shortened eic
+      ### with the smaller rtbounds. need to adjust the function
+      ### findMS2 to look for the right acquisitionScan rather than
+      ### with a which function
       
-      calcfile <- which(rv$filename == input$RAWFileOutput[i])
-      
-      output$status <- renderText({paste("Working on file ", input$RAWFileOutput[i], sep = "")})
-      
-      pdf(file = paste(input$outputdir, "\\", rv$filename[calcfile], "_PlottedEICs.pdf", sep = ""))
-      
-      
-      ms <- try(openMSfile(rv$filepath[calcfile]))
-      hd <- try(header(ms))
 
-      sapply(rv$masses, try(function(x) 
-        {
-        index <- which(rv$masses == x);
-        try(xic(ms, x, width = mzWindow(x, input$masstol), points = TRUE, main = rv$features[index]));
-        try(abline(v = as.numeric(as.character(rv$rts_sec[index])), col = "blue", lwd = 2))
-        })
-        )
-      
-      dev.off()
-      
-      if(i == length(rv$allcalc)){
-        
-        output$status <- renderText({paste("Finished building pdfs")})
-      }
+      eic <- chromatogram(rv$feic.ms1, rt = c(min(rtime(rv$feic.ms1)),max(rtime(rv$feic.ms1))), mz = ppm(rv$masses[process.feature], input$masstol, l = TRUE))
+      plot(eic)
+      abline(v = rv$rts_sec[process.feature], col = "blue")
+      if(is.na(ms2.data)){legend("topleft", legend = paste("No MS2 scans"))}
+      points(ms2.data, col = "red", pch = 16)
+
     }
-      
+
     })
+
+    # output$EIC <- renderPlot({
+    #
+    #   xic(rv$feic, mz = rv$masses[process.feature],
+    #      # rtlim = c(rtlim.small, rtlim.large),
+    #       width = mzWindow(rv$masses[process.feature], input$masstol),
+    #      points = TRUE, legend = TRUE, main = rv$features[process.feature])
+    #   abline(v = rv$rts_sec[process.feature], col = "blue", lwd = 2)
+    #
+    # }, height = 1000, width = 1200
+    # )
+
+        output$Compound <- renderText({paste(rv$names[process.feature])})
+        output$status <- renderText({"Finished"})
+        
+  }
+  })
+
+  # 
+  # observeEvent(c(input$RAWFileOutput,input$selectall), {
+  #   
+  #   if(input$selectall == 0){
+  #     rv$allcalc <- input$RAWFileOutput
+  #     
+  #   }else if(input$selectall%%2 == 0){
+  #     updateCheckboxGroupInput(session, "RAWFileOutput", choices = rv$filename)
+  #     rv$allcalc <- input$RAWFileOutput
+  #     
+  #   }else{
+  #     updateCheckboxGroupInput(session, "RAWFileOutput", choices = rv$filename, selected = rv$filename)
+  #     rv$allcalc <- input$RAWFileOutput
+  #     
+  #   }
+  #   
+  # })
+  # 
+  # 
+  # observeEvent(input$process,{
+  #   
+  #   #output.dir <- input$outputdir
+  #   
+  #   for(i in 1:length(rv$allcalc)){
+  #     
+  #     calcfile <- which(rv$filename == input$RAWFileOutput[i])
+  #     
+  #     output$status <- renderText({paste("Working on file ", input$RAWFileOutput[i], sep = "")})
+  #     
+  #     pdf(file = paste(input$outputdir, "\\", rv$filename[calcfile], "_PlottedEICs.pdf", sep = ""))
+  #     
+  #     
+  #     ms <- try(openMSfile(rv$filepath[calcfile]))
+  #     hd <- try(header(ms))
+  # 
+  #     sapply(rv$masses, try(function(x) 
+  #       {
+  #       index <- which(rv$masses == x);
+  #       try(xic(ms, x, width = mzWindow(x, input$masstol), points = TRUE, main = rv$features[index]));
+  #       try(abline(v = as.numeric(as.character(rv$rts_sec[index])), col = "blue", lwd = 2))
+  #       })
+  #       )
+  #     
+  #     dev.off()
+  #     
+  #     if(i == length(rv$allcalc)){
+  #       
+  #       output$status <- renderText({paste("Finished building pdfs")})
+  #     }
+  #   }
+  #     
+  #   })
 
 }
 
